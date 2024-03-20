60680c976b789a7e48b0167de53f99538f79e883
shahriarislam <shahriarislamrafi71@gmail.com>
2024-03-20 15:27:48 +0600

    change navbar Image

--- src/component/nav/Navbar.jsx
+++ src/component/nav/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
                     <h1 className='md:font-bold md:text-3xl'>Knowledge Cafe</h1>
                 </div>
                 <div>
-                    <img src="../../../public/images/profile.png" alt="" />
+                    <img className='w-14 h-14 rounded-full' src="https://img.freepik.com/premium-photo/portrait-smile-man-with-positive-confidence-carefree-against-grey-studio-background-face-male-person-human-with-cheerful-attitude-freedom-model-with-joy-canada-relax_590464-177008.jpg?w=740" alt="" />
                 </div>
                 
             </div>
